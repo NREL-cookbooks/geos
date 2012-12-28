ddaac063e371fbee04d937a3e20e7c7a31d385fc
Nick Muerdter <nick.muerdter@nrel.gov>
2012-12-27 20:09:20 -0700

    Add geos cookbook.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "NREL"
+maintainer_email "nick.muerdter@nrel.gov"
+license          "MIT"
+description      "Installs/Configures geos"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: geos
+# Recipe:: default
+#
+
+include_recipe "yum::epel"
+
+package "geos-devel"
